de1f8dba7dd024e55e3237ec55f0b5feb16c85a3
ducky502 <wadih.john.jarrouj@gmail.com>
2025-08-25 09:27:55 -0600

    commit changes

---
+++ royal-gutter-solutions/script.js
@@ -0,0 +1,8 @@
+function showSidebar() {
+const sidebar = document.querySelector('.nav-sidebar')
+sidebar.style.display = 'flex';
+}
+function closeSidebar() {
+const sidebar = document.querySelector('.navigation-sidebar')
+sidebar.style.display = 'none';
+}
